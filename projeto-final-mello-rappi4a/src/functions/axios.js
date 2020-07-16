dadc253073427d22f6af0ea2e7b74c8b98a16e68
Lourenço Passos <lo.passos93@gmail.com>
2020-07-16 15:44:22 -0300

    página de detalhe finalizada

--- projeto-final-mello-rappi4a/src/functions/axios.js
+++ projeto-final-mello-rappi4a/src/functions/axios.js
@@ -1,23 +1,34 @@
 import axios from "axios";
 
-const baseUrl = "https://us-central1-missao-newton.cloudfunctions.net/rappi4A"
+const baseUrl = "https://us-central1-missao-newton.cloudfunctions.net/rappi4A";
 
 export const signup = async (body) => {
-    try {
-        const response = await axios.post(`${baseUrl}/signup`, body)
-        return response.data
-    } catch (error) {
-        return error.response.data
-
-    }
-}
+  try {
+    const response = await axios.post(`${baseUrl}/signup`, body);
+    return response.data;
+  } catch (error) {
+    return error.response.data;
+  }
+};
 
 export const login = async (body) => {
-    try {
-        const response = await axios.post(`${baseUrl}/login`, body)
-        return response.data
-    } catch (error) {
-        return error.response.data
-    }
-} 
+  try {
+    const response = await axios.post(`${baseUrl}/login`, body);
+    return response.data;
+  } catch (error) {
+    return error.response.data;
+  }
+};
 
+export const fetchRestaurantDetail = async (restaurantId, token) => {
+  try {
+    const response = await axios.get(`${baseUrl}/restaurants/${restaurantId}`, {
+      headers: {
+        auth: token,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    return error.response.data;
+  }
+};
