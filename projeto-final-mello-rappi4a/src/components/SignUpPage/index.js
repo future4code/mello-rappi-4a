00994897f8698a27bbfabbb9f320df17c2a0da74
Eros Pinder Valente <eros.pv@gmail.com>
2020-07-16 21:08:54 -0300

    refatorando função de validar senha

--- projeto-final-mello-rappi4a/src/components/SignUpPage/index.js
+++ projeto-final-mello-rappi4a/src/components/SignUpPage/index.js
@@ -49,12 +49,12 @@ function SignUpPage() {
 
   const checkEqualPasswords = (event) => {
     handleFormChange(event)
-    if(event.target.value !== form.password) {
+    if(event.target.value !== form.password && event.target.value !== form.confirmPassword) {
       setEqualPasswords(false)
     } else {
       setEqualPasswords(true)
     }
-    handleFormChange(event)
+    
   }
 
   const handleFormSubmit = async (event) => {
